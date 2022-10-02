be728fc7840d4845ab5f88267224886e056a57b8
Jenny Huang <chenyuh565@gmail.com>
2022-10-02 12:51:48 +0800

    create project

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $('.nav-right .nav-link').click(function() {
+    var targetId = $(this).attr('href');
+    var targetPosition = $(targetId).offset().top;
+    var duration = 50;
+    var navbarHeight = $('.nav-container').outerHeight();
+    $('html, body').stop().animate({
+      scrollTop: targetPosition - navbarHeight
+    }, duration);
+  })
+});
+
+
+$(window).on('scroll', function() {
+  if($(window).scrollTop()) {
+    $('.article-nav-bg').addClass('sm');
+  } else {
+    $('.article-nav-bg').removeClass('sm')
+  }
+})
+
+
+// $(document).scroll(function() {
+//   var leftBottomPsition = $('.fixed-left-lg').offset().top + $('.fixed-left-lg').height();
+//   var footerBorder = $('.footer').offset().top; 
+
+//   console.log("leftBottomPsition" + leftBottomPsition)
+//   console.log("footerBorder"+ footerBorder)
+//   if (leftBottomPsition >= footerBorder - 200)
+//     $('.fixed-left-lg').removeClass('fixed');
+
+//   if (leftBottomPsition < footerBorder)
+//     $('.fixed-left-lg').addClass('fixed'); // restore when you scroll up
+// });
+
+$(document).on("click", ".youtube-video-player", function(){ $(".a").click(); });
+
+
